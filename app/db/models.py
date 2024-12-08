5312b3f07eb9cb267e19d568f444157a0ff45e44
Max Dupley <maksimqwe42@mail.ru>
2024-12-08 18:48:16 +0300

    modified:   app/db/models.py 	new file:   zametki/README.md

--- app/db/models.py
+++ app/db/models.py
@@ -5,16 +5,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 # Модель пользователя (User)
-class User(db.Model, UserMixin):
-    __tablename__ = 'user'
+class Account(db.Model, UserMixin):
+    __tablename__ = 'account'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False, index=True)  # Электронная почта
-    phone_number = db.Column(db.String(20), nullable=True)                      # Номер телефона
-    photo = db.Column(db.String(256), nullable=True)                            # Фото
-    password_hash = db.Column(db.String(256), nullable=False)                   # Хеш пароля
-    role = db.Column(db.String(50), nullable=False, default='student')          # Роль пользователя
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)                # Дата создания
+    email = db.Column(db.String(150), unique=True, nullable=False, index=True)              # Электронная почта
+    phone_number = db.Column(db.String(20), nullable=True)                                  # Номер телефона
+    photo = db.Column(db.String(256), nullable=True)                                        # Фото
+    password_hash = db.Column(db.String(256), nullable=False)                               # Хеш пароля
+    role = db.Column(db.String(50), nullable=False, default='student')                      # Роль пользователя
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)                            # Дата создания
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)  # Дата обновления
 
     # Метод для хеширования пароля
     def set_password(self, password):
@@ -25,11 +26,12 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password_hash, password)
 
     # Связь с результатами
-    results = db.relationship('Result', backref='user', lazy=True)
+    results = db.relationship('Result', backref='account', lazy=True)
 
     # Представление объекта для отладки
     def __repr__(self):
         return f'<User {self.first_name} {self.last_name}>'  # Изменено для отображения имени и фамилии
+
 
 # Модель для результатов (Result)
 class Result(db.Model):
@@ -44,6 +46,7 @@ class Result(db.Model):
     # Представление объекта для отладки
     def __repr__(self):
         return f'<Result {self.olympiad_name} - {self.score}>'
+
 
 # Модель для студентов (Student)
 class Student(db.Model):
@@ -67,20 +70,112 @@ class Student(db.Model):
     def __repr__(self):
         return f'<Student {self.student_name} {self.student_surname}>'
 
+
+# Модель олимпиады
+class Olympiad(db.Model):
+    __tablename__ = 'olympiad'
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)  # Внешний ключ на Subject
+    name = db.Column(db.String(100), nullable=False)                                 # Название олимпиады
+    date = db.Column(db.DateTime, nullable=False)                                    # Дата проведения олимпиады
+    description = db.Column(db.Text, nullable=True)                                  # Описание олимпиады
+    passing_score = db.Column(db.Integer, nullable=True)                             # Проходной балл
+    status = db.Column(db.String(50), nullable=False, default='upcoming')            # Статус олимпиады
+
+    subject = db.relationship('Subject', backref=db.backref('olympiads', lazy=True))
+
+    def __repr__(self):
+        return f'<Olympiad {self.name}>'
+
+
 class OlympiadRegistration(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Связь с пользователем
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)       # Связь с пользователем
     olympiad_id = db.Column(db.Integer, db.ForeignKey('olympiad.id'), nullable=False)  # Связь с олимпиадой
 
     student = db.relationship('User', backref='registrations')
     olympiad = db.relationship('Olympiad', backref='registrations')
 
-class Olympiad(db.Model):
+
+# Модель направления
+class Direction(db.Model):
+    __tablename__ = 'direction'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-    description = db.Column(db.Text)
+    name = db.Column(db.String(100), nullable=False, unique=True) # Название направления
+    description = db.Column(db.String(256), nullable=True)        # Описание направления
 
     def __repr__(self):
-        return f'<Olympiad {self.name}>'
-    
+        return f'<Direction {self.name}>'
+
+
+# Модель предмета
+class Subject(db.Model):
+    __tablename__ = 'subject'
+
+    id = db.Column(db.Integer, primary_key=True)
+    direction_id = db.Column(db.Integer, db.ForeignKey('direction.id'), nullable=False) # Внешний ключ на Direction
+    name = db.Column(db.String(100), nullable=False)                                    # Название предмета
+    description = db.Column(db.Text, nullable=True)                                     # Описание предмета
+    demo_version = db.Column(db.String(256), nullable=True)                             # Демо-версия предмета
+    status = db.Column(db.String(50), nullable=False, default='active')                 # Статус предмета
+
+    direction = db.relationship('Direction', backref=db.backref('subjects', lazy=True))
+
+    def __repr__(self):
+        return f'<Subject {self.name}>'
+
+
+# Модель школы
+class School(db.Model):
+    __tablename__ = 'school'
+
+    id = db.Column(db.Integer, primary_key=True)              # Уникальный идентификатор
+    name = db.Column(db.String(150), nullable=False)          # Название школы
+    address = db.Column(db.String(256), nullable=True)        # Адрес школы
+    phone_number = db.Column(db.String(50), nullable=True)    # Телефон школы
+    email = db.Column(db.String(150), nullable=True)          # Электронная почта
+    website = db.Column(db.String(150), nullable=True)        # Веб-сайт
+    student_capacity = db.Column(db.Integer, nullable=True)   # Вместимость школы
+    established_year = db.Column(db.Integer, nullable=True)   # Год основания
+    description = db.Column(db.Text, nullable=True)           # Описание школы
+
+    def __repr__(self):
+        return f'<School {self.name}>'
+
+# Модель баллов и статусов участников
+class Scores(db.Model):
+    __tablename__ = 'scores'
+
+    id = db.Column(db.Integer, primary_key=True)                  # Уникальный идентификатор
+    passing_score = db.Column(db.Integer, nullable=False)         # Проходной балл
+    participant_status = db.Column(db.String(50), nullable=False) # Статус участника
+    # Статус участника должен быть одним из: 'Участник', 'Бронза', 'Серебро', 'Золото'
+    __table_args__ = (
+        db.CheckConstraint(
+            participant_status.in_(['Участник', 'Бронза', 'Серебро', 'Золото']),
+            name='check_participant_status'
+        ),
+    )
+
+    def __repr__(self):
+        return f'<Scores {self.participant_status} - {self.passing_score}>'
+
+# Модель этапов олимпиады
+class OlympiadStages(db.Model):
+    __tablename__ = 'olympiad_stages'
+
+    id = db.Column(db.Integer, primary_key=True)      # Уникальный идентификатор
+    name = db.Column(db.String(100), nullable=False)  # Название этапа
+    status = db.Column(db.String(50), nullable=False) # Статус этапа
+    # Статус этапа должен быть одним из: 'Участник', 'Победитель', 'Финалист', 'Призёр'
+    __table_args__ = (
+        db.CheckConstraint(
+            status.in_(['Участник', 'Победитель', 'Финалист', 'Призёр']),
+            name='check_olympiad_stage_status'
+        ),
+    )
+
+    def __repr__(self):
+        return f'<OlympiadStages {self.name} - {self.status}>'
